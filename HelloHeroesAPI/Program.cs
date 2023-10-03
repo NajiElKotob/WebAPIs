aa8b21a0326a51075199db6e73e0cdeec193d32f
najielkotob <naji@dotnetheroes.com>
2023-10-04 00:55:39 +0400

    Add addhero HTTP method

--- HelloHeroesAPI/Program.cs
+++ HelloHeroesAPI/Program.cs
@@ -1,9 +1,17 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+// Declare the in-memory storage variable here
+List<string> heroesList = new();
+
 
 var app = builder.Build();
 
@@ -17,6 +25,13 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.MapGet("/", () => "Hello Heroes!");
+app.MapGet("/getheroes", () => $"Heroes: {string.Join(", ", heroesList)}!");
+app.MapPost("/addhero",  (string value) =>
+{
+    heroesList.Add(value);
+    return Results.Ok($"'{value}' added to the list of heroes!");
+
+});
 
 
 app.Run();
