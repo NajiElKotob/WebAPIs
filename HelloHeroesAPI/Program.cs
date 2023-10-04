a44e4000f10b18ee142110cff19a3602375e5df8
najielkotob <naji@dotnetheroes.com>
2023-10-04 17:03:21 +0400

    Add DELETE endpoint to remove hero from list

--- HelloHeroesAPI/Program.cs
+++ HelloHeroesAPI/Program.cs
@@ -50,6 +50,19 @@ app.MapGet("/searchhero", (string heroName) =>
 });
 
 
+app.MapDelete("/delhero", (string heroName) =>
+{
+    if (heroesList.Contains(heroName, StringComparer.OrdinalIgnoreCase))
+    {
+        heroesList.Remove(heroName);
+        return Results.Ok($"'{heroName}' removed from the list of heroes!");
+    }
+    else
+    {
+        return Results.NotFound($"{heroName} not found in the list of heroes.");
+    }
+});
+
 app.Run();
 
 
